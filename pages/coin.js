72b7376fc95b263fa55553fc0e213cafcadddf29
Kervin Philippe <kervin@Breeanas-MacBook-Pro.local>
2022-05-06 21:48:03 -0400

    Added the coin page

--- pages/coin.js
+++ pages/coin.js
@@ -1,8 +1,18 @@
 import React from 'react'
 
+import Navbar from '../components/Landing/Utils/Navbars/PrimaryNav'
+
+import styles from '../styles/LandingPage/Coin/Coin.module.css'
+
 function coin() {
   return (
-    <div>coin</div>
+    <div className={styles.container}>
+      <Navbar />
+      <h1 className={styles.title}>BLACK OWNED, NFT COMMUNITY COMBATTING STUDENT LOAN DEBT</h1>
+      <div className={styles.box}>
+        <p className={styles.currency}>1 sle coin = 1.5 eth</p>
+      </div>
+    </div>
   )
 }
 
