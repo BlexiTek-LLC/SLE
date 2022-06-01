00a0a57aea65ee7da0ac3da2f96ce1207fcddd93
Kervin Philippe <kervdev21@gmail.com>
2022-06-01 11:48:49 -0400

    Testing music player again

--- components/Landing/Utils/SoundBar.js
+++ components/Landing/Utils/SoundBar.js
@@ -6,7 +6,7 @@ import style from '../../../styles/LandingPage/Home/Home.module.css'
 
 function SoundBar() 
 {
-    const music = new Audio('/assets/music/Dedication.mp3');
+    const music = new Audio('./assets/music/Dedication.mp3');
     const [click, setClick] = useState(false);
 
     const handleClick = () => {
