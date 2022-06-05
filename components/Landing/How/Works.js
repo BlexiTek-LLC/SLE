7f305358f5805dc0526bbc679a564fc327477c84
mastermindmel <97862081+Melly32@users.noreply.github.com>
2022-06-05 18:40:21 -0500

    added text to hexigons on H.I.W page, changed colors from orange to gold on About page

--- components/Landing/How/Works.js
+++ components/Landing/How/Works.js
@@ -5,30 +5,35 @@ import styles from '../../../styles/LandingPage/Work/Work.module.css'
 function Works() {
   return (
     <div>
-        <h1 className={styles.title}>BLACK OWNED, NFT COMMUNITY COMBATTING STUDENT LOAN DEBT</h1>
+      <h1 className={styles.title}>BLACK OWNED, NFT COMMUNITY COMBATTING STUDENT LOAN DEBT</h1>
 
-        <div className={styles.top}>
-            <div className={styles.hexi}>
-                   
-            </div>
-
-            <div className={styles.hexi}>
-                   
-            </div>
-
-            <div className={styles.hexi}>
-                   
-            </div>
+      <div className={styles.top}>
+        <div className={styles.hexi}>
+          <h1 className={styles.hextext}>Step 1</h1>
+          <h2 className={styles.hexparagraph}>Create your Account <br></br> on the SLE MarketPlace</h2>
         </div>
-        <div className={styles.bottom}>
-            <div className={styles.hexi2}>
-                    
-                
-            </div>
-            <div className={styles.hexi2}>
-                   
-            </div>
+
+        <div className={styles.hexi}>
+          <h1 className={styles.hextext}>Step 3</h1>
+          <h2 className={styles.hexparagraph}>Place for Sell or <br></br> Auction to Gain SLE Tokens</h2>
         </div>
+
+        <div className={styles.hexi}>
+          <h1 className={styles.hextext}>Step 5</h1>
+          <h2 className={styles.hexparagraph}>Contact approved vendor <br></br>to pay using SLE Tokens,<br></br> if not an approved vendor,<br></br> convert tokens to USD<br></br> to make payments</h2>
+        </div>
+      </div>
+      <div className={styles.bottom}>
+        <div className={styles.hexi2}>
+          <h1 className={styles.hextext}>Step 2</h1>
+          <h2 className={styles.hexparagraph}>Upload your <br></br>Non-Fungible Token</h2>
+
+        </div>
+        <div className={styles.hexi2}>
+          <h1 className={styles.hextext}>Step 4</h1>
+          <h2 className={styles.hexparagraph}>With the help<br></br> of the community,<br></br> allow tokens to accumalate to <br></br> match or exceed <br></br> your student debt</h2>
+        </div>
+      </div>
     </div>
   )
 }
